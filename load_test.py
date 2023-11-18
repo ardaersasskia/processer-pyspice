6af7ff63f8b362539a8fa34a10aa6491d89f18a9
Ardaer <yuanxiaowei135@163.com>
2023-11-19 01:10:36 +0800

    2.4.2 debug for load_test.py

--- load_test.py
+++ load_test.py
@@ -2,21 +2,21 @@
 Author: Ardaer yuanxiaowei135@163.com
 Date: 2023-11-19 00:46:22
 LastEditors: Ardaer yuanxiaowei135@163.com
-LastEditTime: 2023-11-19 01:02:13
+LastEditTime: 2023-11-19 01:07:34
 FilePath: \processer-pyspice\load_test.py
 Description: testbench for cpu load, 2 processes now
 
 Copyright (c) 2023 by Ardaer, All Rights Reserved. 
 '''
 
-from multiprocessing import Processes
+from multiprocessing import Process
 import math
 
 def task():
     tmp=7.0
     while True:
         tmp=tmp**math.e
-        if tmp >= 100000:
+        if tmp >= 1.7976931348623157e+100:
             tmp=7
 if __name__=='__main__':
     p1=Process(target=task)
